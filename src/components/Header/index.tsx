929b186edf80f1d4c4ba0aff7a1b0391e4a55b00
carlosdoria <carlosdoria953@gmail.com>
2021-07-15 17:44:21 -0300

    style: create the styles of the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,25 @@
+import Link from 'next/link'
+import { FaGithub } from 'react-icons/fa'
+
 import styles from './styles.module.scss'
 
 export function Header () {
   return (
     <header className={styles.header}>
-      <div className={styles.content}>
-        <h1 className={styles.header__logo}>Header</h1>
-      </div>
+      <Link href='/'>
+        <a className={styles.header__logo}>
+          <FaGithub
+            size={50}
+          />
+        </a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
